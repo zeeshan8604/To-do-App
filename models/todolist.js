1e5cef66c340ff48fc75cd6830214e12b26e1177
Zeeshan Ahmed <zeeshanahmed860443@gmail.com>
2023-06-23 21:37:23 +0530

    final commit

--- models/todolist.js
+++ models/todolist.js
@@ -1,18 +1,18 @@
-const mongoose =require('mongoose');
-const todoSchema =new mongoose.Schema({
+const mongoose = require('mongoose');
+const todoSchema = new mongoose.Schema({
     desc:{
         type:String,
         required:true
     },
     category:{
         type:String,
-        required :true
+        required:true
     },
     dueDate:{
         type:String,
         required:true
-    }
+    }   
 })
 
-const Todolist=mongoose.model('Todolist', todoSchema);
-module.exports=Todolist;
+const TodoLists = mongoose.model('TodoLists', todoSchema);
+module.exports  = TodoLists;
